92c567b6b58e9680aeb9a87d34990e3efb950815
abubakiir7 <miraxmedovabubakr@gmail.com>
2024-06-10 15:59:40 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,619 @@
+<template>
+  <header>
+    <div class="container">
+      <!-- HEAD -->
+
+      <div class="head_section">
+        <div class="head_logo">
+          <img src="../public/Logo.svg" alt="logo" />
+        </div>
+        <div class="head_menu">
+          <a href="#">Home</a>
+          <a href="#">Brands</a>
+          <a href="#">Recent Products</a>
+          <a href="#">Contact</a>
+          <a href="#">About</a>
+        </div>
+        <div class="head_right">
+          <img src="../public/search.svg" alt="search" />
+          <img src="../public/user.svg" alt="user" />
+          <img src="../public/shop.svg" alt="shop" />
+        </div>
+      </div>
+
+      <!-- MAIN -->
+
+      <div class="main_section">
+        <div class="main_right">
+          <h1>Discover Most Suitable Watches</h1>
+          <p>
+            Find the best, reliable, and cheap smart watches here. We focus on
+            product quality. Here you can find smart watches of almost all
+            brands. So why you are waiting? Just order now!
+          </p>
+          <div class="seaches">
+            <input type="text" placeholder="Find the best brands" />
+            <img src="../public/input_search.svg" alt="" />
+            <button class="main_btn">Search</button>
+          </div>
+        </div>
+        <div class="main_left">
+          <img src="../public/main_watch.svg" alt="main_watch" />
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- WATCHES -->
+
+  <section>
+    <div class="container">
+      <!-- TOP -->
+
+      <div class="top">
+        <div class="top_box">
+          <img src="../public/section_top_watch.svg" alt="watch" />
+          <div class="text">
+            <h2>Apple</h2>
+            <p>
+              Apple is one of the most famous smart watches providing company.
+            </p>
+          </div>
+        </div>
+        <div class="top_box">
+          <img src="../public/section_top_watch.svg" alt="watch" />
+          <div class="text">
+            <h2>Xiaomi</h2>
+            <p>Xiaomi smart watches are produced by MI company.</p>
+          </div>
+        </div>
+        <div class="top_box">
+          <img src="../public/section_top_watch.svg" alt="watch" />
+          <div class="text">
+            <h2>FitBit</h2>
+            <p>
+              FitBit smart watches are best for there health and fitness
+              features.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <!-- Our Latest Products -->
+
+      <div class="partially_top_text">
+        <p>Find your favourite smart watch.</p>
+        <h2>Our Latest Products</h2>
+      </div>
+
+      <!-- SEVERAL WATCHES -->
+
+      <div class="middle">
+        <div class="middle_box">
+          <img src="../public/middle_watch.svg" alt="middle_watch" />
+          <h2>Apple Smart I</h2>
+          <div class="div_star">
+            <img class="stars" src="../public/stars.svg" alt="" />
+          </div>
+          <div class="cost">
+            <p>$300.00</p>
+            <p>$255.00</p>
+          </div>
+        </div>
+        <div class="middle_box">
+          <img src="../public/middle_watch.svg" alt="middle_watch" />
+          <h2>Apple Smart I</h2>
+          <div class="div_star">
+            <img class="stars" src="../public/stars.svg" alt="" />
+          </div>
+          <div class="cost">
+            <p>$300.00</p>
+            <p>$255.00</p>
+          </div>
+        </div>
+        <div class="middle_box">
+          <img src="../public/middle_watch.svg" alt="middle_watch" />
+          <h2>Apple Smart I</h2>
+          <div class="div_star">
+            <img class="stars" src="../public/stars.svg" alt="" />
+          </div>
+          <div class="cost">
+            <p>$300.00</p>
+            <p>$255.00</p>
+          </div>
+        </div>
+        <div class="middle_box">
+          <img src="../public/middle_watch.svg" alt="middle_watch" />
+          <h2>Apple Smart I</h2>
+          <div class="div_star">
+            <img class="stars" src="../public/stars.svg" alt="" />
+          </div>
+          <div class="cost">
+            <p>$300.00</p>
+            <p>$255.00</p>
+          </div>
+        </div>
+        <div class="middle_box">
+          <img src="../public/middle_watch.svg" alt="middle_watch" />
+          <h2>Apple Smart I</h2>
+          <div class="div_star">
+            <img class="stars" src="../public/stars.svg" alt="" />
+          </div>
+          <div class="cost">
+            <p>$300.00</p>
+            <p>$255.00</p>
+          </div>
+        </div>
+        <div class="middle_box">
+          <img src="../public/middle_watch.svg" alt="middle_watch" />
+          <h2>Apple Smart I</h2>
+          <div class="div_star">
+            <img class="stars" src="../public/stars.svg" alt="" />
+          </div>
+          <div class="cost">
+            <p>$300.00</p>
+            <p>$255.00</p>
+          </div>
+        </div>
+      </div>
+
+      <!-- BUTTON -->
+
+      <div class="middle_btn">
+        <button>View More</button>
+      </div>
+
+      <!-- PEOPLE -->
+
+      <div class="people">
+        <p>Here are our some of the best clients.</p>
+        <h2>What People Say About Us</h2>
+        <div class="people_comment">
+          <div class="left">
+            <img src="../public/l_people.svg" alt="" />
+            <div class="text">
+              <h2>Hamza Faizi</h2>
+              <p>
+                Don’t waste time, just order! This is the best website to
+                puschase smart watches.
+              </p>
+              <img src="../public/stars.svg" alt="" />
+            </div>
+          </div>
+          <div class="left">
+            <img src="../public/r_people.svg" alt="" />
+            <div class="text">
+              <h2>Hafiz Huzaifa</h2>
+              <p>
+                I’ve been purchasing smart watches of Mohid for a long time. All
+                the products are good quality.
+              </p>
+              <img src="../public/stars.svg" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- END -->
+
+      <div class="end">
+        <div class="text_of_end">
+          <h2>Subscribe To Newsletter</h2>
+          <p>Get free guide about smart watches daily.</p>
+          <div class="seaches">
+            <input type="text" placeholder="Enter Email Address" />
+            <button class="main_btn">Subscribe</button>
+          </div>
+        </div>
+        <img src="../public/middle_watch.svg" alt="" />
+      </div>
+    </div>
+  </section>
+
+  <!-- FOOTER -->
+
+  <footer>
+    <div class="container">
+      <h2>Copyright @ 2022 | Mohid</h2>
+      <div class="text"></div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+header {
+  width: 100%;
+  background: linear-gradient(155.28deg, #383638 2.06%, #0b0b0b 33.08%);
+}
+.container {
+  padding: 0 9%;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+header .container .head_section {
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+header .container .head_section .head_menu {
+  display: flex;
+  gap: 27px;
+}
+
+header .container .head_section .head_menu a {
+  text-decoration: none;
+  color: #8b8e99;
+  font-family: "Poppins", sans-serif;
+  font-weight: 500;
+  font-style: normal;
+}
+
+header .container .head_section .head_right {
+  display: flex;
+  align-items: center;
+  gap: 15.5px;
+}
+
+/*  --------------------  HEAD  ---------------- */
+
+header .main_section {
+  margin-bottom: 200px;
+  margin-top: 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 183px;
+}
+
+header .main_section .main_right h1 {
+  color: #fff;
+  font-family: "Poppins", sans-serif;
+  font-weight: 700;
+  font-style: normal;
+  font-size: 64px;
+  text-align: left;
+}
+
+header .main_section .main_right .seaches {
+  margin-top: 47px;
+  position: relative;
+}
+header .main_section .main_right p {
+  margin-top: 25px;
+  color: #8b8e99;
+  font-family: "Poppins", sans-serif;
+  font-weight: 500;
+  font-style: normal;
+  font-size: 16px;
+  text-align: left;
+}
+header .main_section .main_right input {
+  width: 100%;
+  font-size: 16px;
+
+  background: #fff;
+  border: none;
+  border-radius: 15px;
+  padding: 20px 86px 20px 56px;
+}
+header .main_section .main_right input::placeholder {
+  color: rgba(139, 142, 153, 0.7);
+  font-family: "Poppins", sans-serif;
+  font-weight: 500;
+  font-style: normal;
+  font-size: 16px;
+}
+
+header .main_section .main_right img {
+  position: absolute;
+  left: 0;
+  z-index: 10;
+  width: 20px;
+  height: 20px;
+  top: 17px;
+  left: 21px;
+  color: #000;
+}
+header .main_section .main_right button {
+  background: #3858d6;
+  padding: 10px 25px;
+  border: none;
+  border-radius: 10px;
+  position: absolute;
+  z-index: 10;
+  top: 4.5px;
+  right: 6.5px;
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+  font-style: normal;
+  font-size: 18px;
+  color: #fff;
+}
+
+/*   ---------------------- MAIN  -----------------------  */
+
+section .top {
+  margin-top: 73px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+section .top .top_box {
+  width: 354px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 26px;
+  padding: 46px 41px;
+}
+
+section .top .top_box .text h2 {
+  color: #1e1d1d;
+  font-family: "Inter", sans-serif;
+  font-weight: 700;
+  font-size: 36px;
+}
+section .top .top_box .text p {
+  margin-top: 9px;
+  font-family: "Inter", sans-serif;
+  font-weight: 500;
+  color: #8b8e99;
+}
+
+/* ----------------- Our Latest Products  --------------------- */
+
+section .partially_top_text {
+  margin-top: 135px;
+  width: 100%;
+}
+
+section .partially_top_text p {
+  color: #3858d6;
+  font-family: "Inter", sans-serif;
+  font-weight: 500;
+  text-align: center;
+}
+
+section .partially_top_text h2 {
+  color: #1e1d1d;
+  font-family: "Inter", sans-serif;
+  font-weight: 700;
+  font-size: 36px;
+  text-align: center;
+}
+
+/* ----------------- middle ------------------ */
+
+section .middle {
+  margin-top: 65px;
+  column-gap: 49px;
+  row-gap: 64px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
+
+section .middle .middle_box {
+  padding: 29px 41px;
+}
+
+section .middle .middle_box h2 {
+  margin-top: 39px;
+  color: #1e1d1d;
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+  font-style: normal;
+  font-size: 24px;
+  text-align: center;
+}
+section .middle .middle_box .div_star,
+section .middle .middle_box .cost {
+  margin-top: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+section .middle .middle_box .cost {
+  gap: 3px;
+}
+
+section .middle .middle_box .cost p:first-child {
+  color: #8b8e99;
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+  font-style: normal;
+  font-size: 20px;
+  text-decoration: line-through;
+}
+section .middle .middle_box .cost p:last-child {
+  color: #000;
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+  font-style: normal;
+  font-size: 24px;
+}
+
+/* -------------- SEVERAL WATCHES ---------------- */
+
+section .middle_btn {
+  margin-top: 69px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
+
+section .middle_btn button {
+  background: #3858d6;
+  padding: 9px 40px;
+  border-radius: 10px;
+  border: none;
+  color: #fff;
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+  font-style: normal;
+  font-size: 18px;
+}
+
+/* ---------------- BUTTON ------------------ */
+
+section .people {
+  width: 100%;
+  margin-top: 135px;
+}
+
+section .people p {
+  text-align: center;
+  color: #3858d6;
+  font-family: "Inter", sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+}
+section .people h2 {
+  margin-top: 8px;
+  text-align: center;
+  color: #1e1d1d;
+  font-family: "Inter", sans-serif;
+  font-weight: 700;
+  font-size: 36px;
+}
+
+section .people .people_comment {
+  margin-top: 55px;
+  display: flex;
+  align-items: center;
+  gap: 49px;
+}
+
+section .people .people_comment .left {
+  padding: 30px 34px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 29px;
+}
+
+section .people .people_comment .left .text h2 {
+  color: #1e1d1d;
+  text-align: left;
+  font-family: "Inter", sans-serif;
+  font-weight: 700;
+  font-size: 24px;
+}
+
+section .people .people_comment .left .text p {
+  color: #8b8e99;
+  text-align: left;
+  font-family: "Inter", sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+}
+
+section .people .people_comment .left .text img {
+  margin-top: 9px;
+}
+
+/* ------------ PEOPLE ----------------- */
+
+section .end {
+  margin-top: 135px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 35px 110px;
+}
+
+section .end .text_of_end h2 {
+  font-family: "Inter", sans-serif;
+  font-weight: 700;
+  font-size: 36px;
+  color: #1e1d1d;
+}
+
+section .end .text_of_end p {
+  margin-top: 12px;
+  color: #8b8e99;
+  text-align: left;
+  font-family: "Inter", sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+}
+
+section .end .text_of_end .seaches {
+  margin-top: 20px;
+  position: relative;
+}
+
+section .end .text_of_end .seaches input {
+  width: 100%;
+  background: #fff;
+  font-size: 16px;
+  border: none;
+  border-radius: 15px;
+  padding: 20px 86px 20px 56px;
+}
+
+section .end .text_of_end .seaches input::placeholder {
+  color: rgba(139, 142, 153, 0.7);
+  font-family: "Poppins", sans-serif;
+  font-weight: 500;
+  font-style: normal;
+  font-size: 16px;
+}
+
+section .end .text_of_end .seaches button {
+  background: #3858d6;
+  padding: 10px 25px;
+  border: none;
+  border-radius: 10px;
+  position: absolute;
+  z-index: 10;
+  top: 4.5px;
+  right: 6.5px;
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+  font-style: normal;
+  font-size: 18px;
+  color: #fff;
+}
+
+/* ------------- END ---------------- */
+
+footer {
+  margin-top: 91px;
+  height: 65px;
+  background: #0b0b0b;
+}
+
+footer .container {
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+}
+
+footer h2 {
+  color: #fff;
+  font-family: "Poppins", sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  font-style: normal;
+}
+
+/* ------------ FOOTER ----------- */
+</style>
